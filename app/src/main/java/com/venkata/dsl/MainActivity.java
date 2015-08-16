e24a37922d7cb154615632b03180fb9d4573f3f2
Venkata Buddhiraju <buddhira@adobe.com>
2015-08-15 17:52:56 -0700

    Add custom action bar and images

--- app/src/main/java/com/venkata/dsl/MainActivity.java
+++ app/src/main/java/com/venkata/dsl/MainActivity.java
@@ -1,19 +1,35 @@
 package com.venkata.dsl;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //set the toolbar
+        setupToolBar();
     }
 
+    public void setupToolBar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(mToolbar);
+
+        //show menu icon
+        final ActionBar mActionBar = getSupportActionBar();
+        mActionBar.setDisplayHomeAsUpEnabled(true);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
